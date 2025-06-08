fa88654bd80a73d11be06d68b8b21f23ebb8e013
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-06-08 15:55:23 +0530

    added

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+require('dotenv').config();
 const cors = require("cors")
 const EmployeeModel = require("./models/Employee")
 
@@ -7,7 +8,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.post("/login", (req, res) => {
     const {email, password} = req.body;
